64661e2963aa01bae7f715c0974a589ed9730b41
aswinsbabu <aswinsbabu007@gmail.com>
2022-10-03 22:34:02 +0100

    Update imshow.m

--- imshow.m
+++ imshow.m
@@ -1,7 +1,17 @@
 image= imread("/user/HS400/as03510/Tutorials/cv/lab/cvprlab/testimages/sphinx.jpg");
-imshow(image);
+imgshow(image);
 axis equal;
-size(image);
+%size(image);
 fprintf('%i\n',size(image));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+p= image(3,2,:); %rgb VALUE of pixel 2,3
+r=image(3,2,1);
+g=image(3,2,2);
+b=image(3,2,3);
+
+fprintf('%i \n',p);
+fprintf('r=%i \n g=%i  \n b=%i',r,g,b);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %thresholded = (image >0.15);
 %imshow(thresholded);
